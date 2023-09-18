69d087ca7f967f193724049aa9d0b5c4d8c1ee4e
Robert Johnson <robert.za.johnson@gmail.com>
2023-09-18 11:09:50 -0400

    try catch

--- CollatzAlgorithm.py
+++ CollatzAlgorithm.py
@@ -4,18 +4,22 @@ print("Enter a number: ")
 num = input()
 
 def collatz(n):
-    steps = 0 
-    while n != 1:
-        steps += 1
-        print(steps, "\t", n)
-        if n % 2 == 0:
-            n = int(n / 2)
-        else: 
-            n = n * 3 + 1
+    try:
+        n = int(n)
+        steps = 0 
+        while n != 1:
+            steps += 1
+            print(steps, "\t", n)
+            if n % 2 == 0:
+                n = int(n / 2)
+            else: 
+                n = n * 3 + 1
+    except ValueError:
+        print('Error you did not enter a number')
 
 tracemalloc.start()
 
-collatz(int(num))
+collatz(num)
 
 stats = tracemalloc.get_traced_memory()
 print("Memory usage:", stats[0] / (1024 * 1024), "MB")
